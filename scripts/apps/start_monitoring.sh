c0bc49bc0bde2b7b021f7ee55ce55bf27887d115
aku <akuritsin@gmail.com>
2021-02-09 17:41:20 +0300

    impoved monitoring script

--- scripts/apps/start_monitoring.sh
+++ scripts/apps/start_monitoring.sh
@@ -4,3 +4,5 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH/../../monitoring
 
 docker-compose up -d
+
+echo "Please open http://grafana.local:8080 in your browser"
