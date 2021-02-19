0fe01b1b28f0e985c7edbe1d03d40a6587b4bdc8
aku <akuritsin@gmail.com>
2021-02-19 10:22:01 +0300

    Use custom port to avoid confusion with log output when running multiple instances

--- scripts/apps/start_app1.sh
+++ scripts/apps/start_app1.sh
@@ -2,4 +2,4 @@
 
 clear
 
-docker run -p 3001:3000 -e APP_NAME=app1 --rm --init itprosvet/traefik_tutorial_webapp:latest
+docker run -p 3001:3001 -e APP_NAME=app1 -e PORT=3001 --rm --init itprosvet/traefik_tutorial_webapp:latest
--- scripts/apps/start_app2.sh
+++ scripts/apps/start_app2.sh
@@ -2,4 +2,4 @@
 
 clear
 
-docker run -p 3002:3000 -e APP_NAME=app2 --rm --init itprosvet/traefik_tutorial_webapp:latest
+docker run -p 3002:3002 -e APP_NAME=app2 -e PORT=3002 --rm --init itprosvet/traefik_tutorial_webapp:latest
