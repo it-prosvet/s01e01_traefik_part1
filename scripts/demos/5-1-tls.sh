bdbef25f41dd046e23bfc6afff961ee9fce76a1c
aku <akuritsin@gmail.com>
2021-02-24 22:36:08 +0300

    improved demo scripts - app will be started/stopped automatically

--- scripts/demos/5-1-tls.sh
+++ scripts/demos/5-1-tls.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+CONTAINER_ID=$(docker run -p 3001:3001 -e APP_NAME=app1 -e PORT=3001 --init -d itprosvet/traefik_tutorial_webapp:latest)
+trap 'docker rm -f $CONTAINER_ID 2&>/dev/null' SIGINT SIGTERM EXIT
+
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH/../../traefik
 
--- scripts/demos/6-1-tcp.sh
+++ scripts/demos/6-1-tcp.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+CONTAINER_ID=$(docker run -p 3030:3030 -e PORT=3030 --rm --init itprosvet/traefik_tutorial_tcpserver:latest)
+trap 'docker rm -f $CONTAINER_ID 2&>/dev/null' SIGINT SIGTERM EXIT
+
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH/../../traefik
 
